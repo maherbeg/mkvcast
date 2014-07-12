507fd7d842e809f877c4a264bb8f2665fb4ea168
Maher Beg <maherbeg@gmail.com>
2014-07-11 22:58:14 -0700

    Start file streaming directly from the list page
    Sends the file to stream to a Chromecast without leaving the current
    page.

--- static/js/chromecast.js
+++ static/js/chromecast.js
@@ -12,3 +12,50 @@ var initializePlayer = function(onSessionJoined) {
 
   chrome.cast.initialize(apiConfig, noop, noop);
 };
+
+var onSessionJoined = function(joinedSession) {
+  if (!joinedSession) {
+    return;
+  }
+
+  addStreamStartHandlers(joinedSession);
+
+  if (!joinedSession.media) { return; }
+
+  var mediaSession = joinedSession.media[0];
+  setInterval(onMediaUpdate.bind(null, mediaSession), 1000);
+};
+
+// TODO include start time
+var onMediaUpdate = function(mediaSession) {
+  document.getElementById('cur-time').innerText = convertSecondsToTime(mediaSession.getEstimatedTime());
+
+  var streamingPath = mediaSession.media.contentId;
+  var streamStringStart = '/stream/';
+  var streamStringStartIndex = streamingPath.indexOf(streamStringStart);
+  if (streamStringStartIndex !== -1) {
+    streamingPath = streamingPath.slice(streamStringStartIndex + streamStringStart.length + 1);
+  }
+
+  document.getElementById('now-playing').innerText = streamingPath;
+};
+
+var loadMedia = function(session, event) {
+  var mediaInfo = new chrome.cast.media.MediaInfo(event.target.href, 'video/mkv');
+  var request = new chrome.cast.media.LoadRequest(mediaInfo);
+  request.autoPlay = true;
+
+  request.currentTime = 0;
+
+  session.loadMedia(request);
+};
+
+var addStreamStartHandlers = function(session) {
+
+  var loadMediaForSession = loadMedia.bind(null, session);
+  var allLinks = document.getElementsByTagName('a');
+
+  Array.prototype.forEach.call(allLinks, function(val, index) {
+      val.addEventListener('click', loadMediaForSession);
+  });
+};
