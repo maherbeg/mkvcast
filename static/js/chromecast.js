d5105af3d93bec7265484ae5ae5e063c1dc8d5bd
Maher Beg <maherbeg@gmail.com>
2014-07-12 22:38:08 -0700

    Request session immediately on page load

--- static/js/chromecast.js
+++ static/js/chromecast.js
@@ -70,14 +70,14 @@ var onSessionJoined = function(joinedSession) {
 };
 
 var initializePlayer = function(onSessionJoined) {
-  if (!chrome.cast || !chrome.cast.isAvailable) {
-    setTimeout(initializePlayer.bind(null, onSessionJoined), 1000);
-    return;
-  }
+    if (!chrome.cast || !chrome.cast.isAvailable) {
+        setTimeout(initializePlayer.bind(null, onSessionJoined), 1000);
+        return;
+    }
 
-  var sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID);
+    var sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID);
 
-  var apiConfig = new chrome.cast.ApiConfig(sessionRequest, onSessionJoined, noop, chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED);
+    var apiConfig = new chrome.cast.ApiConfig(sessionRequest, onSessionJoined, noop, chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED);
 
-  chrome.cast.initialize(apiConfig, noop, noop);
+    chrome.cast.initialize(apiConfig, chrome.cast.requestSession.bind(null, noop, noop), noop);
 };
